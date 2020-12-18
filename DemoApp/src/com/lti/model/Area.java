dccbc45c9c89cb582ef316b21fe463edef443ba5
Kaustubha17 <kaustubhashah@gmail.com>
2020-12-18 16:06:41 +0530

    First commit

---
+++ DemoApp/src/com/lti/model/Area.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+public class Area {
+
+	public void Area() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,13 @@
+package com.lti.model;
+
+public class Sample {
+
+	public void show() {
+		System.out.print("Hello world");
+	}
+
+public void method() {
+	System.out.print("Hello All,how are you doing?");
+}
+
+}

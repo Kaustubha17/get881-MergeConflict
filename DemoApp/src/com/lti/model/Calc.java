b528a0cdafe2809cb0fefe4648ba83c4e371067b
Kaustubha17 <kaustubhashah@gmail.com>
2020-12-18 16:41:08 +0530

    Modified sum

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,6 +2,9 @@ package com.lti.model;
 
 public class Calc {
 public void add(int num1,int num2) {
-	System.out.print("Summation="+num1+num2);
-}	
+	System.out.print("Summation="+(num1+num2));
 }
+
+
+}
+

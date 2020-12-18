2099d427de72735888c9ec19339bfcdb57673af8
Kaustubha17 <76148907+Kaustubha17@users.noreply.github.com>
2020-12-18 16:46:32 +0530

    Multiplication method added

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,8 @@ public class Calc {
 public void add(int num1,int num2) {
 	System.out.print("Summation="+(num1+num2));
 }	
+	public void multi(int num1,int num2) {
+	System.out.print("Multiplication="+(num1*num2));
+}	
 
 }

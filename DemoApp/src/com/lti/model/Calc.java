12e976960ee8db9fa7a9e8906b7bc546a55470a9
Kaustubha17 <76148907+Kaustubha17@users.noreply.github.com>
2020-12-18 16:52:23 +0530

    Added fact method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -7,5 +7,14 @@ public void add(int num1,int num2) {
 	public void multi(int num1,int num2) {
 	System.out.print("Multiplication="+(num1*num2));
 }	
+	
+	public void fact(int num){
+	int n=1;
+		while(num>1){
+		n=n*num;
+			num--;
+		}
+		System.out.print(n);
+	}
 
 }
